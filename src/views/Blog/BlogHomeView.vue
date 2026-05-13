1bebaec995d473e9b7a2862efde9526eed91da44
BiancaJoselli <biancajoselli.ifc@gmail.com>
2026-05-13 15:17:17 -0300

    feat: update home page with blog hero section

--- src/views/Blog/BlogHomeView.vue
+++ src/views/Blog/BlogHomeView.vue
@@ -1,7 +1,7 @@
-<script setup>
-import blogHero from '@/components/blog/blogHero.vue';
+<script setup lang="ts">
+import BlogHero from '@/components/blog/blogHero.vue'
 </script>
-<template>
-<blogHero/>
 
+<template>
+  <BlogHero />
 </template>
