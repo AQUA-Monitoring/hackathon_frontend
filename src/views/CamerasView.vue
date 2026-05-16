3cb0627ef5d3a4f9a894fd173e4b53ac05c9946d
matheusmichalski <matheus.michalskifc@gmail.com>
2026-05-16 11:11:35 -0300

    feat: add Cameras view and grid component for camera monitoring

---
+++ src/views/CamerasView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import CamerasGrid from '@/components/camera/camerasGrid.vue';
+import { useCamerasMonitoring } from '@/composables/useCamerasMonitoring';
+
+const { items } = useCamerasMonitoring();
+
+</script>
+
+
+<template>
+  <div>
+    <h1>Câmeras</h1>
+    <CamerasGrid :cameras="items" />
+  </div>
+</template>
