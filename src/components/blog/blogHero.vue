016f91e8121e7de782d6ef2dfcef71d06b0f654d
nicolefemello <nicfmello@gmail.com>
2026-07-03 10:33:06 -0300

    fix: update blogHero component to use dynamic banner URL

--- src/components/blog/blogHero.vue
+++ src/components/blog/blogHero.vue
@@ -1,7 +1,14 @@
+<script setup lang="ts">
+const urlBanner =
+  'https://api-aqua.michalski.app/media/images/4bb61b9a-b42d-4c9e-9622-7ca8a1cd53df.png'
+</script>
+
 <template>
   <div
     class="min-h-100 md:h-135 rounded-b-[8vw] bg-cover bg-center flex items-center"
-    :style="{ backgroundImage: `url('/blog/banner.svg')` }"
+    :style="{
+      backgroundImage: `url(${urlBanner})`,
+    }"
   >
     <div class="text-white px-8 md:px-14">
       <h1 class="grid text-6xl md:text-7xl lg:text-8xl font-bold leading-[1.1]">
