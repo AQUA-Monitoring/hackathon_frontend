6a9f2a26ca41c49730ed7dd569dac4d36eb8b166
matheus.michalskifc@gmail.com <matheus.michalskifc@gmail.com>
2026-07-19 23:44:19 -0300

    fix(frontend): remove sensitive response logging

--- src/stores/Payment.ts
+++ src/stores/Payment.ts
@@ -1,29 +1,28 @@
-import { defineStore } from "pinia";
-import PaymentAPI from "@/services/Payment";
-import type { IPaymentPix, IPaymentCard } from "@/types/payment";
-import { ref } from "vue";
+import { ref } from 'vue'
+import { defineStore } from 'pinia'
+import PaymentAPI from '@/services/Payment'
+import type { IPaymentCard, IPaymentPix } from '@/types/payment'
 
 export const usePaymentStore = defineStore('payment', () => {
-    const paymentApi = new PaymentAPI();
+  const paymentApi = new PaymentAPI()
 
-    const paymentCheckout = ref(null);
+  const paymentCheckout = ref<IPaymentPix | IPaymentCard | null>(null)
 
-    const createPixPayment = async (payment: Partial<IPaymentPix>) => {
-        const data = await paymentApi.createPixPayment(payment)
-        console.log("data: ", data)
-        paymentCheckout.value = data
-        return data
-    }
+  const createPixPayment = async (payment: Partial<IPaymentPix>) => {
+    const data = await paymentApi.createPixPayment(payment)
+    paymentCheckout.value = data
+    return data
+  }
 
-    const createCardPayment = async (payment: Partial<IPaymentCard>) => {
-        const data = await paymentApi.createCardPayment(payment)
-        paymentCheckout.value = data
-        return data
-    }
+  const createCardPayment = async (payment: Partial<IPaymentCard>) => {
+    const data = await paymentApi.createCardPayment(payment)
+    paymentCheckout.value = data
+    return data
+  }
 
-    return {
-        paymentCheckout,
-        createPixPayment,
-        createCardPayment
-    }
+  return {
+    paymentCheckout,
+    createPixPayment,
+    createCardPayment,
+  }
 })
